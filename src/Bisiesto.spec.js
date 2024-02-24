07b3e5a1ad5cbc65237e8faa0f2b39f1e964fa8d
Agustin Ledezma <agustin.ledezma@ucb.edu.bo>
2024-02-23 21:30:49 -0400

    Todos los años divisibles por 100 pero no por 400 NO son años bisiestos

--- src/Bisiesto.spec.js
+++ src/Bisiesto.spec.js
@@ -5,6 +5,10 @@ describe("Bisiesto", () => {
     it("Debería generar que todos los años divisibles entre 400 son bisiestos (por ejemplo, el 2000)", () => {
         expect(esBisiesto(2000)).toEqual(true);
     });
+
+    it("Debería generar que Todos los años divisibles por 100 pero no por 400 NO son años bisiestos (1800)", () => {
+        expect(esBisiesto(1800)).toEqual(false);
+    });
     
 });
 
